7af164b1e99d163d1a1d970faaa2a774d73bfdbc
Vyacheslav Gudkov <vyacheslav.gudkov@dsr-company.com>
2017-09-12 09:16:23 +0300

    * C headers boilerplate added
    * Debian package files added
    
    Signed-off-by: Vyacheslav Gudkov <vyacheslav.gudkov@dsr-company.com>

---
+++ libindy-crypto/include/indy_crypto_bls.h
@@ -0,0 +1,15 @@
+#ifndef __indy__crypto__bls__included__
+#define __indy__crypto__bls__included__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /// TODO: FIXME: Probide list of interfaces for ffi/bls.rs
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
