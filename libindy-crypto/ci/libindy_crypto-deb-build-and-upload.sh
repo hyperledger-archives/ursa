11c1693e07ff86809bc0813838212f16f5362a45
artem.ivanov <artem.ivanov@dsr-company.com>
2017-09-26 08:45:03 +0300

    Updated script for building deb indy-crypto  files
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- libindy-crypto/ci/libindy_crypto-deb-build-and-upload.sh
+++ libindy-crypto/ci/libindy_crypto-deb-build-and-upload.sh
@@ -15,16 +15,16 @@ number="$4"
 [ -z $type ] && exit 3
 [ -z $number ] && exit 4
 
+sed -i -E -e 'H;1h;$!d;x' -e "s/libindy-crypto ([(,),0-9,.]+)/libindy-crypto ($version-$number)/" debian/changelog
+
 dpkg-buildpackage -tc
 
-#sed -i -E -e 'H;1h;$!d;x' -e "s/libindy-crypto ([(,),0-9,.]+)/libindy-crypto ($version-$number)/" debian/changelog
-
-cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
-mkdir /var/repository/repos/libindy_crypto/ubuntu/$type/$version-$number
-cd /var/repository/repos/libindy_crypto/ubuntu/$type/$version-$number
-put -r ../libindy-crypto-dev_"$version"_amd64.deb
-put -r ../libindy-crypto_"$version"_amd64.deb
-ls -l /var/repository/repos/libindy_crypto/ubuntu/$type/$version-$number
-EOF
+#cat <<EOF | sftp -v -oStrictHostKeyChecking=no -i $key repo@192.168.11.111
+#mkdir /var/repository/repos/libindy_crypto/ubuntu/$type/$version-$number
+#cd /var/repository/repos/libindy_crypto/ubuntu/$type/$version-$number
+#put -r ../libindy-crypto-dev_"$version"-"$number"_amd64.deb
+#put -r ../libindy-crypto_"$version"-"$number"_amd64.deb
+#ls -l /var/repository/repos/libindy_crypto/ubuntu/$type/$version-$number
+#EOF
 
 ./sovrin-packaging/upload_debs.py ../ $type
