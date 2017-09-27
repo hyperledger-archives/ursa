7f29175d958d2233f8f4c9e5debf92bf40b33784
artem.ivanov <artem.ivanov@dsr-company.com>
2017-09-27 14:03:41 +0300

    Incremented version
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- wrappers/python/setup.py
+++ wrappers/python/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='indy_crypto',
-    version='0.1.4',
+    version='0.1.5',
     packages=['indy_crypto'],
     url='https://github.com/hyperledger/indy_crypto-crypto',
     license='MIT/Apache-2.0',
