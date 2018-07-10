074a9627473e6fc377afec1692bebe984ede005c
lovesh harchandani <lovesh.bond@gmail.com>
2018-07-10 14:17:16 -0600

    add interface for commitment scheme
    Signed-off-by: lovesh harchandani <lovesh.bond@gmail.com>

--- src/commitments/mod.rs
+++ src/commitments/mod.rs
@@ -1,3 +1,17 @@
+pub mod pedersen;
+
+
 trait CommitmentScheme {
-    // TODO: Define commitment scheme
+    // Returns `num_elements` + 1 generators. This is useful when committing to several messages say
+    // m_1, m_2, m_3, and so on. `setup` will this output g_1, g_2, g_3, g_4 and so on which can then be
+    // used for commitment f(g_1, g_2, g_3, g_4, ..., m_1, m_2, m_3...)
+    // The return type can be changed to Vec<GroupElement> when
+    // we define a `GroupElement` struct
+    fn setup(num_elements: u32) -> Vec<Vec<u8>>;
+
+    // Commits to `messages`. Returns a commitment
+    fn commit(generators: &[&[u8]], messages: &[&[u8]]) -> Vec<u8>;
+
+    // Takes the `secret` and check that the `commitment` was indeed done for the `messages`
+    fn open(commitment: &[u8], secret: &[u8], generators: &[&[u8]], messages: &[&[u8]]) -> bool;
 }
---
+++ src/commitments/pedersen.rs
@@ -0,0 +1,17 @@
+use commitments::CommitmentScheme;
+
+struct PedersenCommitment {}
+
+impl CommitmentScheme for PedersenCommitment {
+    fn setup(num_elements: u32) -> Vec<Vec<u8>> {
+        unimplemented!();
+    }
+
+    fn commit(generators: &[&[u8]], messages: &[&[u8]]) -> Vec<u8> {
+        unimplemented!();
+    }
+
+    fn open(commitment: &[u8], secret: &[u8], generators: &[&[u8]], messages: &[&[u8]]) -> bool {
+        unimplemented!();
+    }
+}
