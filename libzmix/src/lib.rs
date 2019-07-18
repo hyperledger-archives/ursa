3447374eceab919aca172a26aceef2da60af543b
Michael Lodder <redmike7@gmail.com>
2019-07-18 15:47:22 -0600

    Implement FieldOrderElements for BLS12-381
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- libzmix/src/lib.rs
+++ libzmix/src/lib.rs
@@ -3,6 +3,11 @@ extern crate lazy_static;
 extern crate amcl_wrapper;
 extern crate failure;
 extern crate rand;
+#[cfg(test)]
+extern crate rand_chacha;
+extern crate rand_core;
+extern crate sha2;
+extern crate zeroize;
 extern crate serde;
 extern crate sha2;
 extern crate zeroize;
