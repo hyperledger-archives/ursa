5ccd84fa6d6655142d3bb4252f5726c33bc72b38
Michael Lodder <redmike7@gmail.com>
2019-07-30 15:31:01 -0600

    Added G1, G2, Pair
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- libzmix/src/lib.rs
+++ libzmix/src/lib.rs
@@ -1,6 +1,6 @@
 #[macro_use]
 extern crate lazy_static;
-extern crate amcl;
+extern crate amcl_miracl;
 extern crate hkdf;
 extern crate rand;
 #[cfg(test)]
@@ -10,11 +10,6 @@ extern crate serde;
 extern crate sha2;
 extern crate zeroize;
 
-pub mod commitments;
-pub mod ffi;
-pub mod hash_functions;
-pub mod prf;
-pub mod signatures;
 pub mod utils;
 pub mod verifiable_encryption;
 pub mod zkl;
