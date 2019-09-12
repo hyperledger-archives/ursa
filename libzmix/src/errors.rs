797fb58bbed6166abd4b0065248aa5185dc5665a
Michael Lodder <redmike7@gmail.com>
2019-09-12 09:01:07 -0600

    Remove duplicate error
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- libzmix/src/errors.rs
+++ libzmix/src/errors.rs
@@ -5,30 +5,6 @@ pub mod prelude {
     pub use super::{BBSError, BBSErrorExt, BBSErrorKind, PSError};
 }
 
-#[derive(Debug, Fail)]
-pub enum PSError {
-    #[fail(
-        display = "Verkey has unequal number of Y and Y_tilde elements. Y={} and Y_tilde={}",
-        y, y_tilde
-    )]
-    InvalidVerkey { y: usize, y_tilde: usize },
-
-    #[fail(
-        display = "Verkey valid for {} messages but given {} messages",
-        expected, given
-    )]
-    UnsupportedNoOfMessages { expected: usize, given: usize },
-
-    #[fail(
-        display = "Same no of bases and exponents required. {} bases and {} exponents",
-        bases, exponents
-    )]
-    UnequalNoOfBasesExponents { bases: usize, exponents: usize },
-
-    #[fail(display = "Error with message {:?}", msg)]
-    GeneralError { msg: String },
-}
-
 #[derive(Debug, Fail, Clone)]
 pub enum BBSErrorKind {
     #[fail(display = "Key Generation Error")]
