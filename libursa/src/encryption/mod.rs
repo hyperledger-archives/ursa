214ee915fa0afd682ffa3e2ec7d45fff6c3a3567
Michael Lodder <redmike7@gmail.com>
2019-11-21 08:55:39 -0700

    Added more comments
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- libursa/src/encryption/mod.rs
+++ libursa/src/encryption/mod.rs
@@ -6,6 +6,7 @@ use rand::{rngs::OsRng, RngCore};
 
 pub mod symm;
 
+// Helpful for generating bytes using the operating system random number generator
 pub fn random_vec(bytes: usize) -> Result<Vec<u8>, Error> {
     let mut value = vec![0u8; bytes];
     let mut rng = OsRng::new().map_err(|_| Error)?;
