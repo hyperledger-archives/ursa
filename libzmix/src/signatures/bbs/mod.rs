60d4892fbadcf6b8665d166b8228e90000ca0100
Michael Lodder <redmike7@gmail.com>
2020-04-07 06:13:35 -0600

    Add deterministic bbs
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- libzmix/src/signatures/bbs/mod.rs
+++ libzmix/src/signatures/bbs/mod.rs
@@ -3,9 +3,9 @@ pub mod pok_sig;
 pub mod signature;
 
 pub mod prelude {
-    pub use super::keys::{generate, PublicKey, SecretKey};
+    pub use super::keys::prelude::*;
     pub use super::pok_sig::{
         PoKOfSignature, PoKOfSignatureProof, ProofG1, ProverCommittedG1, ProverCommittingG1,
     };
-    pub use super::signature::Signature;
+    pub use super::signature::{Signature, SIGNATURE_SIZE};
 }
