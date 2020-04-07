4e14c8a5baaf20e87314ed7d153d16b1304195a8
Michael Lodder <redmike7@gmail.com>
2020-04-07 06:13:35 -0600

    Add deterministic bbs
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- libzmix/src/signatures/bbs/keys.rs
+++ libzmix/src/signatures/bbs/keys.rs
@@ -2,11 +2,24 @@ use amcl_wrapper::{
     constants::GroupG1_SIZE, errors::SerzDeserzError, field_elem::FieldElement,
     group_elem::GroupElement, group_elem_g1::G1, group_elem_g2::G2, types_g2::GroupG2_SIZE,
 };
+use hash2curve::{bls381g1::Bls12381G1Sswu, HashToCurveXmd};
+use hash2curve::DomainSeparationTag;
 
 use crate::errors::prelude::*;
 
 pub mod prelude {
-    pub use super::{generate, PublicKey, SecretKey};
+    pub use super::{generate, PublicKey, SecretKey, DeterministicPublicKey};
+    pub use hash2curve::DomainSeparationTag;
+    pub use amcl_wrapper::constants::FieldElement_SIZE as SECRET_KEY_SIZE;
+    pub use amcl_wrapper::constants::FieldElement_SIZE as MESSAGE_SIZE;
+    pub use amcl_wrapper::constants::GroupG1_SIZE as COMMITMENT_SIZE;
+    pub use amcl_wrapper::types_g2::GroupG2_SIZE as PUBLIC_KEY_SIZE;
+}
+
+#[derive(Debug, Clone)]
+pub enum KeyGenOption {
+    UseSeed(Vec<u8>),
+    FromSecretKey(Vec<u8>)
 }
 
 // https://eprint.iacr.org/2016/663.pdf Section 4.3
@@ -69,6 +82,75 @@ impl PublicKey {
     }
 }
 
+/// Used to deterministically generate all other generators given a commitment to a private key
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct DeterministicPublicKey {
+    w: G2,
+}
+
+impl DeterministicPublicKey {
+    /// Generates a random `Secretkey` and only creates the commitment to it
+    pub fn new(option: Option<KeyGenOption>) -> (Self, SecretKey) {
+        let secret = match option {
+            Some(ref o) => match o {
+                KeyGenOption::UseSeed(ref v) => FieldElement::from_msg_hash(v.as_slice()),
+                KeyGenOption::FromSecretKey(ref sk) => FieldElement::from_bytes(&sk[..]).unwrap()
+            },
+            None => FieldElement::random()
+        };
+        let w = &G2::generator() * &secret;
+        (Self { w }, secret)
+    }
+
+    /// Convert to a normal public key but deterministically derive all the generators
+    pub fn to_public_key(&self, message_count: usize, dst: DomainSeparationTag) -> PublicKey {
+        let point_hasher = Bls12381G1Sswu::new(dst);
+
+        let mut data = Vec::with_capacity(message_count);
+        data.extend_from_slice(&self.w.to_bytes());
+        data.push(0u8);
+        data.extend_from_slice(&(message_count.clone() as u32).to_be_bytes()[..]);
+        let h0: G1 = point_hasher
+            .hash_to_curve_xmd::<sha2::Sha256>(data.as_slice())
+            .unwrap().0
+            .into();
+        let mut current_h = h0.clone();
+        let mut h = Vec::new();
+        for i in 1..message_count {
+            data.clear();
+            data.extend_from_slice(&current_h.to_bytes());
+            data.push(0u8);
+            data.extend_from_slice(&(i as u32).to_be_bytes()[..]);
+            current_h = point_hasher
+                .hash_to_curve_xmd::<sha2::Sha256>(data.as_slice())
+                .unwrap().0
+                .into();
+            h.push(current_h.clone());
+        }
+
+        PublicKey {
+            w: self.w.clone(),
+            h0,
+            h,
+        }
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        self.w.to_bytes()
+    }
+
+    pub fn from_bytes(data: &[u8]) -> Result<Self, SerzDeserzError> {
+        let w = G2::from_bytes(&data)?;
+        Ok(DeterministicPublicKey { w })
+    }
+}
+
+impl From<G2> for DeterministicPublicKey {
+    fn from(w: G2) -> Self {
+        DeterministicPublicKey { w }
+    }
+}
+
 /// Create a new BBS+ keypair
 pub fn generate(message_count: usize) -> Result<(PublicKey, SecretKey), BBSError> {
     if message_count == 0 {
@@ -115,5 +197,21 @@ mod tests {
         //Check serialization is working
         let public_key_2 = PublicKey::from_bytes(bytes.as_slice()).unwrap();
         assert_eq!(public_key_2, public_key);
+    }
+
+    #[test]
+    fn key_conversion() {
+        let (dpk, _) = DeterministicPublicKey::new(None);
+        let dst = DomainSeparationTag::new(b"TEST", None, None, None).unwrap();
+
+        let pk = dpk.to_public_key(5, dst);
+
+        for i in 0..pk.h.len() {
+            assert_ne!(pk.h0, pk.h[i], "h[0] == h[{}]", i + 1);
+
+            for j in (i+1)..pk.h.len() {
+                assert_ne!(pk.h[i], pk.h[j], "h[{}] == h[{}]", i + 1, j + 1);
+            }
+        }
     }
 }
