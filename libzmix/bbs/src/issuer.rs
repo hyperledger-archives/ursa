c74d5dbb8f200d3e5d26d6f858154fef22147fe5
Michael Lodder <redmike7@gmail.com>
2020-04-10 09:52:02 -0600

    Minor updates
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- libzmix/bbs/src/issuer.rs
+++ libzmix/bbs/src/issuer.rs
@@ -43,7 +43,7 @@ impl Issuer {
         nonce: &SignatureNonce,
     ) -> Result<BlindSignature, BBSError> {
         if ctx.verify(messages, verkey, nonce)? {
-            BlindSignature::new(&ctx.commitment, messages, signkey, verkey)
+            Ok(BlindSignature::new(&ctx.commitment, messages, signkey, verkey))
         } else {
             Err(BBSErrorKind::GeneralError {
                 msg: format!("Invalid proof of committed messages"),
