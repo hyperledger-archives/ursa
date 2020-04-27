9876f3aca77bf76dd44ab9d1565dfe4685da580e
Michael Lodder <redmike7@gmail.com>
2020-04-27 14:26:29 -0600

    Test if messages and signature match before creating proof
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- libzmix/bbs/tests/bbs.rs
+++ libzmix/bbs/tests/bbs.rs
@@ -236,10 +236,10 @@ fn pok_sig_bad_message() {
     let signature = Signature::new(messages.as_slice(), &sk, &pk).unwrap();
 
     let nonce = Verifier::generate_proof_nonce();
-    let proof_request = Verifier::new_proof_request(&[1, 3], &pk).unwrap();
+    let mut proof_request = Verifier::new_proof_request(&[1, 3], &pk).unwrap();
 
     // Sends `proof_request` and `nonce` to the prover
-    let proof_messages = vec![
+    let mut proof_messages = vec![
         pm_hidden!(b"message_0"), //message that wasn't signed
         pm_revealed!(b"message_2"),
         pm_hidden!(b"message_3"),
@@ -247,8 +247,11 @@ fn pok_sig_bad_message() {
         pm_hidden!(b"message_5"),
     ];
 
-    let pok = Prover::commit_signature_pok(&proof_request, proof_messages.as_slice(), &signature)
-        .unwrap();
+    let res = Prover::commit_signature_pok(&proof_request, proof_messages.as_slice(), &signature);
+    assert!(res.is_err());
+    proof_messages[0] = pm_hidden!(b"message_1");
+    let pok = Prover::commit_signature_pok(&proof_request, proof_messages.as_slice(), &signature).unwrap();
+
     let mut challenge_bytes = Vec::new();
     challenge_bytes.extend_from_slice(pok.to_bytes().as_slice());
     challenge_bytes.extend_from_slice(&nonce.to_bytes()[..]);
@@ -256,30 +259,7 @@ fn pok_sig_bad_message() {
     let challenge = SignatureNonce::from_msg_hash(&challenge_bytes);
 
     let proof = Prover::generate_signature_pok(pok, &challenge).unwrap();
-
-    match Verifier::verify_signature_pok(&proof_request, &proof, &nonce) {
-        Ok(_) => assert!(false),
-        Err(_) => assert!(true),
-    };
-
-    // Sends `proof_request` and `nonce` to the prover
-    let proof_messages = vec![
-        pm_revealed!(b"message_0"), //message that wasn't signed
-        pm_revealed!(b"message_2"),
-        pm_hidden!(b"message_3"),
-        pm_revealed!(b"message_4"),
-        pm_hidden!(b"message_5"),
-    ];
-
-    let pok = Prover::commit_signature_pok(&proof_request, proof_messages.as_slice(), &signature)
-        .unwrap();
-    let mut challenge_bytes = Vec::new();
-    challenge_bytes.extend_from_slice(pok.to_bytes().as_slice());
-    challenge_bytes.extend_from_slice(&nonce.to_bytes()[..]);
-
-    let challenge = SignatureNonce::from_msg_hash(&challenge_bytes);
-
-    let proof = Prover::generate_signature_pok(pok, &challenge).unwrap();
+    proof_request.revealed_messages.insert(0);
 
     match Verifier::verify_signature_pok(&proof_request, &proof, &nonce) {
         Ok(_) => assert!(false),
@@ -287,25 +267,15 @@ fn pok_sig_bad_message() {
     };
 
     let proof_request = Verifier::new_proof_request(&[0, 1, 2, 3], &pk).unwrap();
-
-    // Sends `proof_request` and `nonce` to the prover
-    let proof_messages = vec![
-        pm_revealed!(b"message_1"), //message that wasn't signed
-        pm_revealed!(b"message_2"),
-        pm_hidden!(b"message_3"),
-        pm_revealed!(b"message_4"),
-        pm_hidden!(b"message_5"),
-    ];
-
-    let pok = Prover::commit_signature_pok(&proof_request, proof_messages.as_slice(), &signature)
-        .unwrap();
+    let pok = Prover::commit_signature_pok(&proof_request, proof_messages.as_slice(), &signature).unwrap();
     let mut challenge_bytes = Vec::new();
     challenge_bytes.extend_from_slice(pok.to_bytes().as_slice());
     challenge_bytes.extend_from_slice(&nonce.to_bytes()[..]);
 
     let challenge = SignatureNonce::from_msg_hash(&challenge_bytes);
 
-    let proof = Prover::generate_signature_pok(pok, &challenge).unwrap();
+    let mut proof = Prover::generate_signature_pok(pok, &challenge).unwrap();
+    proof.revealed_messages.insert(0, SignatureMessage::from_msg_hash(b"message_1"));
 
     //The proof is not what the verifier asked for
     match Verifier::verify_signature_pok(&proof_request, &proof, &nonce) {
@@ -313,3 +283,4 @@ fn pok_sig_bad_message() {
         Err(_) => assert!(true),
     };
 }
+
