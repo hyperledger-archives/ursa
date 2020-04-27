c1874916544dcf1e476141ede024fc2741bd6a85
Michael Lodder <redmike7@gmail.com>
2020-04-27 11:47:58 -0600

    Add another test for bad prover
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- libzmix/bbs/tests/bbs.rs
+++ libzmix/bbs/tests/bbs.rs
@@ -221,3 +221,68 @@ fn pok_sig_extra_message() {
         Err(_) => assert!(true),
     };
 }
+
+#[test]
+fn pok_sig_bad_message() {
+    let (pk, sk) = Issuer::new_keys(5).unwrap();
+    let messages = vec![
+        SignatureMessage::from_msg_hash(b"message_1"),
+        SignatureMessage::from_msg_hash(b"message_2"),
+        SignatureMessage::from_msg_hash(b"message_3"),
+        SignatureMessage::from_msg_hash(b"message_4"),
+        SignatureMessage::from_msg_hash(b"message_5"),
+    ];
+
+    let signature = Signature::new(messages.as_slice(), &sk, &pk).unwrap();
+
+    let nonce = Verifier::generate_proof_nonce();
+    let proof_request = Verifier::new_proof_request(&[1, 3], &pk).unwrap();
+
+    // Sends `proof_request` and `nonce` to the prover
+    let proof_messages = vec![
+        pm_hidden!(b"message_0"), //message that wasn't signed
+        pm_revealed!(b"message_2"),
+        pm_hidden!(b"message_3"),
+        pm_revealed!(b"message_4"),
+        pm_hidden!(b"message_5"),
+    ];
+
+    let pok = Prover::commit_signature_pok(&proof_request, proof_messages.as_slice(), &signature)
+        .unwrap();
+    let mut challenge_bytes = Vec::new();
+    challenge_bytes.extend_from_slice(pok.to_bytes().as_slice());
+    challenge_bytes.extend_from_slice(&nonce.to_bytes()[..]);
+
+    let challenge = SignatureNonce::from_msg_hash(&challenge_bytes);
+
+    let proof = Prover::generate_signature_pok(pok, &challenge).unwrap();
+
+    match Verifier::verify_signature_pok(&proof_request, &proof, &nonce) {
+        Ok(_) => assert!(false),
+        Err(_) => assert!(true),
+    };
+
+    // Sends `proof_request` and `nonce` to the prover
+    let proof_messages = vec![
+        pm_revealed!(b"message_0"), //message that wasn't signed
+        pm_revealed!(b"message_2"),
+        pm_hidden!(b"message_3"),
+        pm_revealed!(b"message_4"),
+        pm_hidden!(b"message_5"),
+    ];
+
+    let pok = Prover::commit_signature_pok(&proof_request, proof_messages.as_slice(), &signature)
+        .unwrap();
+    let mut challenge_bytes = Vec::new();
+    challenge_bytes.extend_from_slice(pok.to_bytes().as_slice());
+    challenge_bytes.extend_from_slice(&nonce.to_bytes()[..]);
+
+    let challenge = SignatureNonce::from_msg_hash(&challenge_bytes);
+
+    let proof = Prover::generate_signature_pok(pok, &challenge).unwrap();
+
+    match Verifier::verify_signature_pok(&proof_request, &proof, &nonce) {
+        Ok(_) => assert!(false),
+        Err(_) => assert!(true),
+    };
+}
