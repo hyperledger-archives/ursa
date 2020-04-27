da3909f07dabab06323be01f9bdcb584108fe329
Michael Lodder <redmike7@gmail.com>
2020-04-27 11:42:05 -0600

    Add more tests
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- libzmix/bbs/tests/bbs.rs
+++ libzmix/bbs/tests/bbs.rs
@@ -154,3 +154,68 @@ fn pok_sig() {
         Err(_) => assert!(false), // Why did the proof failed
     };
 }
+
+#[test]
+fn pok_sig_extra_message() {
+
+    let (pk, sk) = Issuer::new_keys(5).unwrap();
+    let messages = vec![
+        SignatureMessage::from_msg_hash(b"message_1"),
+        SignatureMessage::from_msg_hash(b"message_2"),
+        SignatureMessage::from_msg_hash(b"message_3"),
+        SignatureMessage::from_msg_hash(b"message_4"),
+        SignatureMessage::from_msg_hash(b"message_5"),
+    ];
+
+    let signature = Signature::new(messages.as_slice(), &sk, &pk).unwrap();
+
+    let nonce = Verifier::generate_proof_nonce();
+    let mut proof_request = Verifier::new_proof_request(&[1, 3], &pk).unwrap();
+
+    // Sends `proof_request` and `nonce` to the prover
+    let proof_messages = vec![
+        pm_hidden!(b"message_1"),
+        pm_revealed!(b"message_2"),
+        pm_hidden!(b"message_3"),
+        pm_revealed!(b"message_4"),
+        pm_hidden!(b"message_5"),
+    ];
+
+    let pok = Prover::commit_signature_pok(&proof_request, proof_messages.as_slice(), &signature).unwrap();
+
+    // complete other zkps as desired and compute `challenge_hash`
+    // add bytes from other proofs
+
+    let mut challenge_bytes = Vec::new();
+    challenge_bytes.extend_from_slice(pok.to_bytes().as_slice());
+    challenge_bytes.extend_from_slice(&nonce.to_bytes()[..]);
+
+    let challenge = SignatureNonce::from_msg_hash(&challenge_bytes);
+
+    let mut proof = Prover::generate_signature_pok(pok, &challenge).unwrap();
+
+    // Reveal a message that was hidden, should fail
+    proof_request.revealed_messages.insert(4);
+
+    // Send `proof` and `challenge` to Verifier
+
+    match Verifier::verify_signature_pok(&proof_request, &proof, &nonce) {
+        Ok(_) => assert!(false),
+        Err(_) => assert!(true),
+    };
+
+    proof_request.revealed_messages.remove(&4);
+    proof.revealed_messages.insert(4, SignatureMessage::from_msg_hash(b"message_4"));
+
+    match Verifier::verify_signature_pok(&proof_request, &proof, &nonce) {
+        Ok(_) => assert!(false),
+        Err(_) => assert!(true),
+    };
+
+    proof.revealed_messages.remove(&4);
+    proof.revealed_messages.insert(3, SignatureMessage::new());
+    match Verifier::verify_signature_pok(&proof_request, &proof, &nonce) {
+        Ok(_) => assert!(false),
+        Err(_) => assert!(true),
+    };
+}
