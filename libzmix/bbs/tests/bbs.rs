1d387b3c0e325e8a5b49f026518bcb2b1f906067
Brent Zundel <brent.zundel@gmail.com>
2020-05-14 11:34:16 -0600

    merge conflicts
    Signed-off-by: Brent Zundel <brent.zundel@gmail.com>

--- libzmix/bbs/tests/bbs.rs
+++ libzmix/bbs/tests/bbs.rs
@@ -270,6 +270,74 @@ fn pok_sig_bad_message() {
 }
 
 #[test]
+fn test_challenge_hash_with_prover_claims() {
+    //issue credential
+    let (pk, sk) = Issuer::new_keys(5).unwrap();
+    let messages = vec![
+        SignatureMessage::from_msg_hash(b"message_1"),
+        SignatureMessage::from_msg_hash(b"message_2"),
+        SignatureMessage::from_msg_hash(b"message_3"),
+        SignatureMessage::from_msg_hash(b"message_4"),
+        SignatureMessage::from_msg_hash(b"message_5"),
+    ];
+
+    let signature = Signature::new(messages.as_slice(), &sk, &pk).unwrap();
+
+    //verifier requests credential
+    let nonce = Verifier::generate_proof_nonce();
+    let proof_request = Verifier::new_proof_request(&[1, 3], &pk).unwrap();
+
+    // Sends `proof_request` and `nonce` to the prover
+    let proof_messages = vec![
+        pm_hidden!(b"message_1"),
+        pm_revealed!(b"message_2"),
+        pm_hidden!(b"message_3"),
+        pm_revealed!(b"message_4"),
+        pm_hidden!(b"message_5"),
+    ];
+
+    // prover creates pok for proof request
+    let pok = Prover::commit_signature_pok(&proof_request, proof_messages.as_slice(), &signature)
+        .unwrap();
+
+    let claims = vec!["self-attested claim1", "self-attested claim2"];
+
+    // complete other zkps as desired and compute `challenge_hash`
+    let challenge =
+        Prover::create_challenge_hash(vec![pok.clone()], claims.clone(), &nonce).unwrap();
+
+    let proof = Prover::generate_signature_pok(pok, &challenge).unwrap();
+
+    // Send `proof`, `claims`, and `challenge` to Verifier
+
+    // Verifier creates their own challenge bytes
+    // and adds proof and claims to it
+    let mut ver_chal_bytes = proof.proof.get_bytes_for_challenge(
+        proof_request.revealed_messages.clone(),
+        &proof_request.verification_key,
+    );
+    for c in claims {
+        ver_chal_bytes.extend_from_slice(c.as_bytes());
+    }
+
+    // Verifier completes ver_challenge_bytes by adding verifier_nonce,
+    // then constructs the challenge
+    ver_chal_bytes.extend_from_slice(&nonce.to_bytes()[..]);
+    let ver_challenge = SignatureNonce::from_msg_hash(&ver_chal_bytes);
+
+    // Verifier checks proof1
+    let res = proof.proof.verify(
+        &proof_request.verification_key,
+        &proof.revealed_messages,
+        &ver_challenge,
+    );
+    match res {
+        Ok(_) => assert!(true),   // check revealed messages
+        Err(_) => assert!(false), // Why did the proof fail?
+    };
+}
+
+#[test]
 fn bbs_demo() {
     // Prover generates link secret
     let link_secret = Prover::new_link_secret();
