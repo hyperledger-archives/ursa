57e4f9580e68bc5bf60369a15b85f152a49c9ed7
Michael Lodder <redmike7@gmail.com>
2020-06-03 22:30:06 -0600

    Cargo fmt
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- libzmix/bbs/src/pok_vc.rs
+++ libzmix/bbs/src/pok_vc.rs
@@ -11,9 +11,10 @@
 //! `Proof` can then be verified by the verifier.
 
 use crate::{
+    errors::{BBSError, BBSErrorKind},
     hash_to_fr, multi_scalar_mul_const_time_g1, Commitment, GeneratorG1, ProofChallenge,
     SignatureMessage, ToVariableLengthBytes, FR_COMPRESSED_SIZE, G1_COMPRESSED_SIZE,
-    G1_UNCOMPRESSED_SIZE, errors::{BBSError, BBSErrorKind}
+    G1_UNCOMPRESSED_SIZE,
 };
 
 use failure::{Backtrace, Context, Fail};
