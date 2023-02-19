277558bb1cd6b54642763fad4738b948cf35f5c9
Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>
2023-02-20 00:22:40 +0100

    [version]: Bump trivial dependencies
    Signed-off-by: Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>

--- libursa/src/hash/blake2.rs
+++ libursa/src/hash/blake2.rs
@@ -1,4 +1,4 @@
 #[cfg(target_pointer_width = "64")]
-pub use crate::blake2::{Blake2b as Blake2, VarBlake2b as VarBlake2};
+pub use crate::blake2::Blake2b as Blake2;
 #[cfg(target_pointer_width = "32")]
-pub use crate::blake2::{Blake2s as Blake2, VarBlake2s as VarBlake2};
+pub use crate::blake2::Blake2s as Blake2;
